88db2e7e7f45b5e574a51d4774a1999999751d03
Davnox900 <68035597+Davnox900@users.noreply.github.com>
2020-07-27 10:31:33 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-let posX = 0
-led.plot(0, posX)
+let posY = 0
+led.plot(0, posY)
 let direction = 1
 let liste = [0, 1, 2, 3, 4]
 basic.forever(function () {
     basic.pause(1000)
-    led.unplot(0, posX)
-    posX += direction
-    if (posX >= 4) {
+    led.unplot(0, posY)
+    posY += direction
+    if (posY >= 4) {
         direction = -1
-    } else if (posX <= 0) {
+    } else if (posY <= 0) {
         direction = 1
     }
-    led.plot(0, posX)
+    led.plot(0, posY)
 })
